59d46be1bb44d1061bbaa808a9185cef5f711b3a
Hyowon Choi <81157265+wonniiii@users.noreply.github.com>
2024-07-13 14:58:29 +0900

    [Gold IV] Title: 문자열 폭발, Time: 524 ms, Memory: 34196 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String str = br.readLine();
+        String bomb = br.readLine();
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            stack.push(str.charAt(i));
+
+            if (stack.size() >= bomb.length()) {
+                boolean isSame = true;
+                for (int j = 0; j < bomb.length(); j++) {
+                    if (stack.get(stack.size() - bomb.length() + j) != bomb.charAt(j)) {
+                        isSame = false;
+                        break;
+                    }
+                }
+
+                if (isSame) {
+                    for (int j = 0; j < bomb.length(); j++)
+                        stack.pop();
+                }
+            }
+        }
+
+        for (char ch : stack)
+            sb.append(ch);
+
+        System.out.println(sb.length() > 0 ? sb.toString() : "FRULA");
+    }
+    }
+
+
+
+
