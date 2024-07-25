9b6c11c39804a334e75410a5979a9fd84e9f0c0c
Hyowon Choi <81157265+wonniiii@users.noreply.github.com>
2024-07-25 19:14:31 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -2,11 +2,11 @@ def solution(n, arr1, arr2):
     answer = []
     str1 = ""
     str2 = ""
-    strn = str(n)+'b'
+    
     for i in range(0,n):
         str_res = ""
-        str1 = format(arr1[i], strn)
-        str2 = format(arr2[i], strn)
+        str1 = format(arr1[i], 'b').zfill(n)
+        str2 = format(arr2[i], 'b').zfill(n)
         
         for a1, a2 in zip(str1, str2):
             if a1 == "1" or a2 == "1":
@@ -16,16 +16,3 @@ def solution(n, arr1, arr2):
         answer.append(str_res)
 
     return answer
-
-def binaryNum(num, n):
-    res = ""
-    while num != 1:
-        op = num % 2
-        res += str(op)
-        num = num // 2
-    res += "1"
-    
-    for _ in range(n-len(res)):
-        res += "0"
-    
-    return res [::-1]
