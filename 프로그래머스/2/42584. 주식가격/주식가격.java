f27ba0c9cd5f3c669057809a0ff76d620e9c3223
Hyowon Choi <81157265+wonniiii@users.noreply.github.com>
2025-06-23 22:04:30 +0900

    [level 2] Title: 주식가격, Time: 41.20 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -6,10 +6,10 @@ class Solution {
         int[] answer = new int[n];
         Stack<Integer> stack = new Stack<>();
         
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i<n; i++) {
             while(!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
                 int j = stack.pop();
-                answer[j] = i-j;
+                answer[j] = i - j;
             }
             stack.push(i);
         }
@@ -18,6 +18,7 @@ class Solution {
             int j = stack.pop();
             answer[j] = n - 1 - j;
         }
+        
         return answer;
     }
 }
