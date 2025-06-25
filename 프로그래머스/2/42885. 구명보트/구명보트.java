a0564f04b8ccd8ad2bec9505b344f4900786e0b4
Hyowon Choi <81157265+wonniiii@users.noreply.github.com>
2025-06-25 21:02:18 +0900

    [level 2] Title: 구명보트, Time: 11.40 ms, Memory: 58.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        int left = 0;
+        int right = people.length - 1;
+        Arrays.sort(people);
+        
+        while(left <= right) {
+            if(people[left] + people[right] <= limit) {
+                right--;
+                left++;
+            } else {
+                right--;
+            }
+            answer++;
+        }
+        
+        return answer;
+    }
+}
