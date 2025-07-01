3141fa79ec2e146aa6082af0b26f8be853c78744
Hyowon Choi <81157265+wonniiii@users.noreply.github.com>
2025-07-01 17:15:38 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.02 ms, Memory: 9.16 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -0,0 +1,9 @@
+def solution(n):
+    battery = 0
+    while n > 0:
+        if n % 2 == 0:
+            n = n // 2  
+        else:
+            n -= 1     
+            battery += 1
+    return battery
