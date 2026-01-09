cf8eaa3fa0f2773aaff817e7b6caac9c22233577
Hyowon Choi <81157265+wonniiii@users.noreply.github.com>
2026-01-09 16:28:05 +0900

    [level 1] Title: 시저 암호, Time: 2.17 ms, Memory: 9.13 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -0,0 +1,10 @@
+def solution(s, n):
+    answer = ''
+    for i in s:
+        if i.isupper():
+            i = chr((ord(i)-ord('A') + n) % 26 + ord('A'))
+        elif i.islower():
+            i = chr((ord(i)-ord('a') + n) % 26 + ord('a'))
+        answer += i
+
+    return answer
